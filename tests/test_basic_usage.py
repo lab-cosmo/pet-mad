ae2e7b14cb8ee409c86717c6760a80ebbbb3206f
Arslan Mazitov <arslan.mazitov@phystech.edu>
2025-04-28 16:57:12 +0200

    Update PET-MAD to v1.1

--- tests/test_basic_usage.py
+++ tests/test_basic_usage.py
@@ -1,9 +1,26 @@
 from pet_mad.calculator import PETMADCalculator
 from ase.build import bulk
+import pytest
 
 
 def test_basic_usage():
     atoms = bulk("Si", cubic=True, a=5.43, crystalstructure="diamond")
     atoms.calc = PETMADCalculator(version="latest")
+    _ = atoms.get_potential_energy()
+    _ = atoms.get_forces()
+
+@pytest.mark.parametrize(
+    "version",
+    [
+        "latest",
+        "1.0.1",
+        "1.0.0",
+        "0.4.1",
+        "0.3.2",
+    ],
+)
+def test_version(version):
+    atoms = bulk("Si", cubic=True, a=5.43, crystalstructure="diamond")
+    atoms.calc = PETMADCalculator(version=version)
     _ = atoms.get_potential_energy()
     _ = atoms.get_forces()
