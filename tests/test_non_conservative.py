c1390a2c0ce14fb0cbf3fec72edd5ca817df47cc
Arslan Mazitov <arslan.mazitov@phystech.edu>
2025-04-28 19:18:19 +0200

    Temporarily disabled NC test

--- tests/test_non_conservative.py
+++ tests/test_non_conservative.py
@@ -2,7 +2,7 @@ from pet_mad.calculator import PETMADCalculator
 from ase.build import bulk
 import pytest
 
-
+@pytest.mark.skip(reason="Waiting until https://github.com/metatensor/metatensor/pull/914 is merged")
 def test_non_conservative():
     atoms = bulk("Si", cubic=True, a=5.43, crystalstructure="diamond")
     atoms.calc = PETMADCalculator(version="latest", non_conservative=True)
