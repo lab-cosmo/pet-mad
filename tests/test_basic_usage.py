9570d7f0db659583447bdcc1286b9da0cddaae6b
Arslan Mazitov <arslan.mazitov@phystech.edu>
2025-04-28 19:43:37 +0200

    Fixed the tests for the deprecated version of PET-MAD

--- tests/test_basic_usage.py
+++ tests/test_basic_usage.py
@@ -14,10 +14,21 @@ def test_basic_usage():
     [
         "latest",
         "1.0.1",
-        "1.0.0",
     ],
 )
 def test_version(version):
+    atoms = bulk("Si", cubic=True, a=5.43, crystalstructure="diamond")
+    atoms.calc = PETMADCalculator(version=version)
+    _ = atoms.get_potential_energy()
+    _ = atoms.get_forces()
+
+@pytest.mark.parametrize(
+    "version",
+    [
+        "1.0.0",
+    ],
+)
+def test_version_deprecated(version):
     atoms = bulk("Si", cubic=True, a=5.43, crystalstructure="diamond")
     atoms.calc = PETMADCalculator(version=version)
     _ = atoms.get_potential_energy()
