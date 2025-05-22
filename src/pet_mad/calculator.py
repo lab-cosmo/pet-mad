690f33df2b8c2b1b931bb684d01eee5c48575643
Guillaume Fraux <guillaume.fraux@epfl.ch>
2025-05-22 13:48:46 +0200

    Directly inherit from MetatensorCalculator

--- src/pet_mad/calculator.py
+++ src/pet_mad/calculator.py
@@ -3,15 +3,11 @@ import logging
 import os
 import warnings
 from platformdirs import user_cache_dir
-from typing import List, Optional
+from typing import Optional
 
-import ase
-import ase.calculators
-import ase.calculators.calculator
 from metatensor.torch.atomistic import ModelMetadata
 from metatensor.torch.atomistic.ase_calculator import MetatensorCalculator
 from metatrain.utils.io import load_model as load_metatrain_model
-
 
 warnings.filterwarnings(
     "ignore",
@@ -43,38 +39,30 @@ BASE_URL = (
 )
 
 
-class PETMADCalculator(ase.calculators.calculator.Calculator):
+class PETMADCalculator(MetatensorCalculator):
     """
     PET-MAD ASE Calculator
-
-    :param version: PET-MAD version to use. Supported versions are "latest",
-    "v1.0.1", "1.0.0". Defaults to "latest".
-    :param checkpoint_path: path to a checkpoint file to load the model from. If
-        provided, the `version` parameter is ignored.
-    :param additional_outputs: Dictionary of additional outputs to be computed by
-        the model. These outputs will always be computed whenever the
-        `calculate` function is called (e.g. by `ase.Atoms.get_potential_energy`,
-        `ase.optimize.optimize.Dynamics.run`, *etc.*) and stored in the
-        `additional_outputs` attribute. If you want more control over when
-        and how to compute specific outputs, you should use `run_model` instead.
-    :param extensions_directory: if the model uses extensions, we will try to load
-        them from this directory
-    :param check_consistency: should we check the model for consistency when
-        running, defaults to False.
-    :param device: torch device to use for the calculation. If `None`, we will try
-        the options in the model's `supported_device` in order.
     """
-
-    implemented_properties = ase.calculators.calculator.all_properties
 
     def __init__(
         self,
         version: str = "latest",
         checkpoint_path: Optional[str] = None,
-        *args,
-        **kwargs,
+        *,
+        check_consistency=False,
+        device=None,
     ):
-        super().__init__()
+        """
+        :param version: PET-MAD version to use. Supported versions are "latest",
+            "v1.0.1", "1.0.0". Defaults to "latest".
+        :param checkpoint_path: path to a checkpoint file to load the model from. If
+            provided, the `version` parameter is ignored.
+        :param check_consistency: should we check the model for consistency when
+            running, defaults to False.
+        :param device: torch device to use for the calculation. If `None`, we will try
+            the options in the model's `supported_device` in order.
+        """
+
         if version not in VERSIONS:
             raise ValueError(
                 f"Version {version} is not supported. Supported versions are {VERSIONS}"
@@ -112,24 +100,11 @@ class PETMADCalculator(ase.calculators.calculator.Calculator):
         logging.info(f"Exporting checkpoint to TorchScript at {pt_path}")
         model.save(pt_path, collect_extensions=extensions_directory)
 
-        self._calculator = MetatensorCalculator(
-            pt_path, *args, non_conservative=False, **kwargs
-        )
-
-    def calculate(
-        self,
-        atoms: ase.Atoms,
-        properties: List[str],
-        system_changes: List[str],
-    ) -> None:
-        self._calculator.calculate(atoms, properties, system_changes)
-        self.results = self._calculator.results
-
-    def compute_energy(
-        self,
-        atoms: ase.Atoms,
-        compute_forces_and_stresses: bool = False,
-    ) -> dict:
-        return self._calculator.compute_energy(
-            atoms, compute_forces_and_stresses=compute_forces_and_stresses
+        super().__init__(
+            pt_path,
+            extensions_directory=extensions_directory,
+            check_consistency=check_consistency,
+            device=device,
+            non_conservative=False,
+            additional_outputs={},
         )
