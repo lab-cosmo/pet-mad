644fb176d4c793e1c0de382ee05a466902c484ef
Arslan Mazitov <arslan.mazitov@phystech.edu>
2025-08-21 14:06:49 +0200

    Suppressed warning in tests

--- tests/pet_mad/test_basic_usage.py
+++ tests/pet_mad/test_basic_usage.py
@@ -6,12 +6,27 @@ import pytest
 
 VERSIONS = ("1.1.0", "1.0.1")
 
+def get_warnings():
+    
+    msg1 = (
+        "trying to upgrade an old model checkpoint with unknown version, this "
+        "might fail and require manual modifications"
+    )
+    msg2 = (
+        "PET assumes that Cartesian tensors of rank 2 are stress-like, meaning that "
+        "they are symmetric and intensive. If this is not the case, please use a "
+        "different model."
+    )
+    return msg1, msg2
+
 @pytest.mark.parametrize(
     "version",
     VERSIONS,
 )
 def test_get_pet_mad(version):
-    model = get_pet_mad(version=version)
+    msg1, msg2 = get_warnings()
+    with pytest.warns(UserWarning, match=f"({msg1}|{msg2})"):
+        model = get_pet_mad(version=version)
     assert model.metadata().name == f"PET-MAD v{version}"
 
 
@@ -21,12 +36,16 @@ def test_get_pet_mad(version):
 )
 def test_save_pet_mad(version, monkeypatch, tmp_path):
     monkeypatch.chdir(tmp_path)
-    save_pet_mad(version=version, output=f"pet-mad-{version}.pt")
+    msg1, msg2 = get_warnings()
+    with pytest.warns(UserWarning, match=f"({msg1}|{msg2})"):
+        save_pet_mad(version=version, output=f"pet-mad-{version}.pt")
     assert os.path.exists(f"pet-mad-{version}.pt")
 
 def test_basic_usage():
     atoms = bulk("Si", cubic=True, a=5.43, crystalstructure="diamond")
-    atoms.calc = PETMADCalculator(version="latest")
+    msg1, msg2 = get_warnings()
+    with pytest.warns(UserWarning, match=f"({msg1}|{msg2})"):
+        atoms.calc = PETMADCalculator(version="latest")
     _ = atoms.get_potential_energy()
     _ = atoms.get_forces()
 
@@ -37,7 +56,9 @@ def test_basic_usage():
 )
 def test_version(version):
     atoms = bulk("Si", cubic=True, a=5.43, crystalstructure="diamond")
-    atoms.calc = PETMADCalculator(version=version)
+    msg1, msg2 = get_warnings()
+    with pytest.warns(UserWarning, match=f"({msg1}|{msg2})"):
+        atoms.calc = PETMADCalculator(version=version)
     _ = atoms.get_potential_energy()
     _ = atoms.get_forces()
 
@@ -50,6 +71,8 @@ def test_version(version):
 )
 def test_version_deprecated(version):
     atoms = bulk("Si", cubic=True, a=5.43, crystalstructure="diamond")
-    atoms.calc = PETMADCalculator(version=version)
+    msg1, msg2 = get_warnings()
+    with pytest.warns(UserWarning, match=f"({msg1}|{msg2})"):
+        atoms.calc = PETMADCalculator(version=version)
     _ = atoms.get_potential_energy()
     _ = atoms.get_forces()
--- tests/pet_mad/test_non_conservative.py
+++ tests/pet_mad/test_non_conservative.py
@@ -1,11 +1,24 @@
 from pet_mad.calculator import PETMADCalculator
 from ase.build import bulk
 import numpy as np
+import pytest
 
 def test_non_conservative():
     atoms = bulk("Si", cubic=True, a=5.43, crystalstructure="diamond")
-    calc = PETMADCalculator(version="latest", non_conservative=False)
-    calc_nc = PETMADCalculator(version="latest", non_conservative=True)
+
+    msg1 = (
+        "trying to upgrade an old model checkpoint with unknown version, this "
+        "might fail and require manual modifications"
+    )
+    msg2 = (
+        "PET assumes that Cartesian tensors of rank 2 are stress-like, meaning that "
+        "they are symmetric and intensive. If this is not the case, please use a "
+        "different model."
+    )
+
+    with pytest.warns(UserWarning, match=f"({msg1}|{msg2})"):
+        calc = PETMADCalculator(version="latest", non_conservative=False)
+        calc_nc = PETMADCalculator(version="latest", non_conservative=True)
 
     atoms.calc = calc
     forces = atoms.get_forces()
