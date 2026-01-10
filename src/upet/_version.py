f968142d1597db072286d74d64a0f70f41382176
Arslan Mazitov <arslan.mazitov@phystech.edu>
2026-01-10 14:55:42 +0100

    Updated the list of available versions

--- src/upet/_version.py
+++ src/upet/_version.py
@@ -1,11 +1,13 @@
 UPET_AVAILABLE_MODELS = [
     "pet-mad-s",
-    "pet-omad-l",
     "pet-omat-xs",
     "pet-omat-s",
     "pet-omat-m",
     "pet-omat-l",
+    "pet-omat-xl",
     "pet-oam-l",
+    "pet-oam-xl",
+    "pet-omad-l",
     "pet-omatpes-l",
     "pet-spice-s",
     "pet-spice-l",
@@ -13,8 +15,8 @@ UPET_AVAILABLE_MODELS = [
 
 UPET_NO_NC_SUPPORT_MODELS = [
     "pet-mad-s-v1.0.2",
-    "pet-spice-s-v1.0.0",
-    "pet-spice-l-v1.0.0",
+    "pet-spice-s-v0.2.0",
+    "pet-spice-l-v0.2.0",
 ]
 
 UPET_UQ_SUPPORTED_MODELS = [
