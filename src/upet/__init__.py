6c02b875c3063f49175452034e86d9281518304c
frostedoyster <bigi.f@libero.it>
2026-03-01 12:21:28 +0100

    Set dynamic JIT fusion strategy

--- src/upet/__init__.py
+++ src/upet/__init__.py
@@ -1,6 +1,14 @@
 __version__ = "0.2.0"
 
+import torch
+
 from ._models import get_upet, save_upet
+
+
+# Disable static fusion. Besides the fact that atomistic batches have variable
+# sizes, statically fused CUDA kernels cannot allocate new tensors at runtime,
+# causing "Global alloc not supported yet" errors (cuda 13+) at the time of writing
+torch.jit.set_fusion_strategy([("DYNAMIC", 10)])
 
 
 __all__ = ["get_upet", "save_upet"]
